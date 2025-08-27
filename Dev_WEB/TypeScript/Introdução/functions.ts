660f58789ced53237eb628ac6b1b3e2120647614
Sid Jr <sidalbertok@gmail.com>
2025-08-27 18:29:26 -0300

    Delete Dev_WEB/TypeScript/Introdução directory

--- Dev_WEB/TypeScript/Introdução/functions.ts
+++
@@ -1,9 +0,0 @@
-function soma(){
-    const n1:number = Number(prompt("Informe um numero ")!);
-    const n2:number = Number(prompt("Informe um numero " )!);
-    const n3:number = Number(prompt("Informe um numero ")!);
-    const n4:number = Number(prompt("Informe um numero ")!);
-    const soma = n1+n2+n3+n4;
-    console.log("Soma será",soma);
-}
-soma()
