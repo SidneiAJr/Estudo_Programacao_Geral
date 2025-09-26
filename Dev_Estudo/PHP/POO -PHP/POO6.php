3846c6718bac9e77e33c215c04cde2b21b8a111c
Sid Jr <sidalbertok@gmail.com>
2025-09-26 11:39:04 -0300

    Update POO6.php

--- Dev_Estudo/PHP/POO -PHP/POO6.php
+++ Dev_Estudo/PHP/POO -PHP/POO6.php
@@ -21,7 +21,7 @@ interface VerificaClasse{
 }
 
 abstract class Classe implements VerificarPersonagem, VerificaHabilidade, VerificaClasse{
-   public function __construct(public string $NomePersonagem,public int $DanoPersonage, public string $InfoPersonagem) {}
+   public function __construct(public string $NomePersonagem,public int $DanoPersonage, public string $InfoPersonagem,public string $tipoIten) {}
 }
 
 class Personagem extends Classe implements VerificaClasse, VerificarPersonagem,VerificaHabilidade{
@@ -40,6 +40,21 @@ class Personagem extends Classe implements VerificaClasse, VerificarPersonagem,V
     public function VerificaLevel(): void
     {
         
+    }
+}
+class Itens implements VerificarItens{
+    public function Itens(): void
+    {
+        
+    }
+    public function ItensCertos(): bool
+    {
+        if($this->$tipoIten=="Espada"){
+           echo "Erro";
+        }else{
+            echo "Item Errado";
+        }
+        return $tipoIten;
     }
 }
 
